61a38a779dc36fca3169a2efb4ca68b26aff75a3
Bohan Chen <bochen@pivotal.io>
2021-05-10 18:02:31 -0400

    cleanup api response
    [#6904]
    
    resource_config_version_id is useful for constructing nodes (if we
    ignore builds)
    
    having the proper version instead of a json string is nicer to render
    
    Signed-off-by: Bohan Chen <bochen@pivotal.io>
    Co-authored-by: Alex Suraci <suraci.alex@gmail.com>

--- web/elm/src/Concourse.elm
+++ web/elm/src/Concourse.elm
@@ -1182,8 +1182,9 @@ decodeVersionedResource =
 
 type alias CausalityResourceVersion =
     { resourceId : Int
+    , versionId : Int
     , resourceName : String
-    , version : String
+    , version : Version
     , inputTo : List CausalityBuild
     }
 
@@ -1206,8 +1207,9 @@ decodeCausalityResourceVersion : Json.Decode.Decoder CausalityResourceVersion
 decodeCausalityResourceVersion =
     Json.Decode.succeed CausalityResourceVersion
         |> andMap (Json.Decode.field "resource_id" Json.Decode.int)
+        |> andMap (Json.Decode.field "resource_version_id" Json.Decode.int)
         |> andMap (Json.Decode.field "resource_name" Json.Decode.string)
-        |> andMap (Json.Decode.field "version" Json.Decode.string)
+        |> andMap (Json.Decode.field "version" decodeVersion)
         |> andMap (defaultTo [] (Json.Decode.field "input_to" (Json.Decode.list decodeCausalityBuild)))
 
 
